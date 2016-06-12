4acf2a68a7a163bf6319dd33616b602ac0a5d8ac
ryanguy426 <ryanguy426@users.noreply.github.com>
2016-06-12 11:42:14 -0400

    Include OpenJDK 7

--- Docs/Scripts/Build_Env_Setup.sh
+++ Docs/Scripts/Build_Env_Setup.sh
@@ -50,7 +50,7 @@ read agree2
         		else exit
 		fi
 sudo apt-get update
-sudo apt-get -y install git-core gnupg flex bison gperf build-essential \
+sudo apt-get -y install openjdk-7-jdk git-core gnupg flex bison gperf build-essential \
   zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
   lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
   libgl1-mesa-dev libxml2-utils xsltproc unzip
